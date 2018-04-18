3d942a850ca2c434bca9efa710a938097fd9afb8
patryckx <eng,patrickmonteiro@gmail.com>
2018-04-18 18:15:18 -0300

    correção na responsividade

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Estoque Unama</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
 	<link rel="stylesheet"  href="<?= base_url("dist/css/bootstrap.min.css") ?>">
 	<script rel="stylesheet"   src="<?= base_url("dist/fontAwesome/js/fontawesome-all.js") ?>"></script>
@@ -29,19 +30,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	  </div>
 	</nav>
 
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row justify-content-md-center">
-			<div class="col col-lg-4 text-center">
+			<div class="col col-lg-4 col-xs-12 text-center">
 				<i class="fas fa-cube fa-8x" style="color: grey;"></i>
 
-				<div class="input-group mb-3">
+				<div class="input-group pt-4 input-group-lg">
 				  <div class="input-group-prepend">
 				    <span class="input-group-text" id="basic-addon1">@</span>
 				  </div>
 				  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
 				</div>
 
-				<div class="input-group mb-3">
+				<div class="input-group pt-4 input-group-lg">
 				  <div class="input-group-prepend">
 				    <span class="input-group-text" id="basic-addon1">@</span>
 				  </div>
@@ -53,7 +54,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-	
+
 	<script src="<?= base_url("dist/js/bootstrap.min.js")?>"></script>
 
 
